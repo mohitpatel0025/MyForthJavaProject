b1c799cd912ee409de4011155edc6f5be54a002c
Mohit Patel <mohit0025@yahoo.com>
2024-04-29 21:38:03 +0100

    edit the file

--- MyForthClass.java
+++ MyForthClass.java
@@ -5,6 +5,6 @@ public class MyForthClass {
     public static void main( String[] args){
 
         System.out.println( " Mohit's Forth Project");
-        
+        System.out.println( " this is change in file");
     }
 }
